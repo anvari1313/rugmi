c4e371cba53073b5b8bca8001f058e9f477d05e6
Ahmad Anvari <ahmad.anv@gmail.com>
2020-09-16 10:37:00 +0430

    Make access token persistent

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -2,7 +2,7 @@ import api from '../../api/imgur';
 import qs from 'qs';
 
 const state = {
-    token: null
+    token: window.localStorage.getItem('imgur_access_token')
 };
 
 const getters = {
@@ -15,11 +15,14 @@ const actions = {
     },
     finalizeLogin: ({commit}, hash) => {
         const query = qs.parse(hash.replace('#', ''));
+        const accessToken = query['access_token']
 
-        commit('setToken', query['access_token']);
+        commit('setToken', accessToken);
+        window.localStorage.setItem('imgur_access_token', accessToken);
     },
     logout: ({commit}) => {
         commit('setToken', null);
+        window.localStorage.setItem('imgur_access_token', null);
     }
 };
 
