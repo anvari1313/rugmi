810d68f226072871a98d0692b4564202f3f25267
Ahmad Anvari <ahmad.anv@gmail.com>
2020-09-16 09:40:11 +0430

    Add simple login command

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="ui secondary pointing menu">
+    <a class="active item">
+      Rugmi
+    </a>
+    <a class="item">
+      Messages
+    </a>
+    <a class="item">
+      Friends
+    </a>
+    <div class="right menu">
+      <a class="ui item" @click="login">
+        Login
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+
+export default {
+  name: 'AppHeader',
+  methods: mapActions(['login'])
+}
+</script>
