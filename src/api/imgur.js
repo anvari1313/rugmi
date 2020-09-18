e88c97e96aeb29497d084d64d77d3c7df6cb6502
Ahmad Anvari <ahmad.anv@gmail.com>
2020-09-18 09:10:48 +0430

    Implement fetch api method

--- src/api/imgur.js
+++ src/api/imgur.js
@@ -1,4 +1,5 @@
 import qs from 'qs';
+import axios from 'axios';
 
 const CLIENT_ID = 'f97feace0d52421';
 const BASE_URL = 'https://api.imgur.com';
@@ -11,5 +12,17 @@ export default {
         };
 
         window.location = `${BASE_URL}/oauth2/authorize?${qs.stringify(querystring)}`
+    },
+
+    fetchIMages(token) {
+        const config = {
+            method: 'get',
+            url: `${BASE_URL}/3/account/me/images`,
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        };
+
+        return axios(config);
     }
 };
